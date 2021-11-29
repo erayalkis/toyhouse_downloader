3279431730603c067cbc9f5a241c81734c627d47
Lily <lolpo12345@outlook.com>
2021-11-29 21:59:38 +0300

    Create app form

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ function App() {
       <div className="card p-4">
         <p className="display-4">Toyhouse Downloader</p>
         <p>Mass download directly from Toyhouse</p>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control form-control-lg" placeholder="Toyhouse Link" />
-          <div class="input-group-append">
-            <button class="btn btn-outline-primary btn-lg" type="button">Button</button>
-          </div>
-        </div>
+
       </div>
     </div>
   );
---
+++ src/AppForm.jsx
@@ -0,0 +1,14 @@
+
+const AppForm = () => {
+
+  return(
+    <div class="input-group mb-3">
+      <input type="text" class="form-control form-control-lg" placeholder="Toyhouse Link" />
+      <div class="input-group-append">
+        <button class="btn btn-outline-primary btn-lg" type="button">Button</button>
+      </div>
+    </div>
+  )
+};
+
+export default AppForm;
