83d1cf4e0ff5609bdf664170bfdcfa43bbd27be3
Lily <lolpo12345@outlook.com>
2021-11-30 19:17:12 +0300

    Change app name

--- src/Limitations.jsx
+++ src/Limitations.jsx
@@ -3,12 +3,12 @@ const Limitations = () => {
 
   return(
     <div className="mt-5">
-      <h1 className="display-4 text-center">Limitations for Toyhou.dl</h1>
+      <h1 className="display-4 text-center">Limitations for this app</h1>
       <div className="mx-auto mt-4" style={{width:500}}>
         <ul>
-          <li>Toyhou.dl can't fetch galleries for locked (private) profiles.</li>
-          <li>Toyhou.dl can't fetch images that have NSFW tags on them.</li>
-          <li>Toyhou.dl can't fetch galleries that have custom designs.</li>
+          <li>This app can't fetch galleries for locked (private) profiles.</li>
+          <li>This app can't fetch images that have NSFW tags on them.</li>
+          <li>This app can't fetch galleries that have custom designs.</li>
         </ul>   
       </div>
       
