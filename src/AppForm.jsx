43881c71d78a4e453cac1c355631eb8a361f86eb
Lily <lolpo12345@outlook.com>
2022-01-29 00:15:00 +0300

    Revert "Remove unused code"
    This reverts commit 13536deb25d4fc9731a17f9b2f04547a16e9d7b9.

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -1,82 +1,87 @@
+import JSZip from "jszip";
+import { saveAs } from 'file-saver';
 import { useState } from "react";
 
 const AppForm = (props) => {
   const [queryStr, setQueryStr] = useState("");
 
   const handleSubmit = () => {
-    props.setHasError(false);
+    props.setHasError(false)
 
     let id = null;
-    if (!queryStr.startsWith("https") && !queryStr.startsWith("toyhou.se")) {
-      props.setHasError("Please paste in a valid Toyhouse link!");
-      return;
-    } else if (queryStr.startsWith("toyhou.se")) {
-      id = queryStr.split("/")[1];
-    } else if (queryStr.startsWith("https://toyhou.se")) {
-      id = queryStr.split("/")[3];
+    if(!queryStr.startsWith('https') && !queryStr.startsWith('toyhou.se')) {
+      props.setHasError("Please paste in a valid Toyhouse link!")
+      return
+    } else if(queryStr.startsWith('toyhou.se')) {
+      id = queryStr.split("/")[1]
+    } else if(queryStr.startsWith('https://toyhou.se')) {
+      id = queryStr.split("/")[3]
     } else {
-      props.setHasError("Please paste in a valid Toyhouse link!");
-      return;
+      props.setHasError("Please paste in a valid Toyhouse link!")
+      return
     }
 
-    props.setLoading("Downloading images...");
-    fetch(`https://toyhouse-rails-api.herokuapp.com/cache_gallery/?id=${id}`)
-      .then((response) => {
-        return response.json();
+    props.setLoading("Downloading images...")
+    fetch(`https://toyhouse-rails-api.herokuapp.com/character/?id=${id}&gallery_only=true`)
+      .then(response => { 
+        return response.json()
       })
-      .then((response) => {
-        if (response.status === 422 || response.status === 404) {
+      .then(response => {
+        if(response.status === 422 || response.status === 404) {
           props.setLoading(null);
           props.setHasError(response.msg);
         } else {
-          window.location = `https://toyhouse-rails-api.herokuapp.com/download_gallery/?id=${id}`;
-          setQueryStr("");
-          props.setLoading("Gallery downloaded!");
-          setTimeout(() => {
-            props.setLoading(null);
-          }, 3000);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        props.setLoading(null);
-        props.setHasError(
-          "An error occured while downloading your gallery! :("
-        );
+          props.setLoading("Handling the gallery...")
+          let zip = new JSZip();
+          const promises = [];
 
-        setTimeout(() => {
-          props.setHasError(null);
-          setQueryStr("");
-        }, 2000);
+        response.gallery.forEach(async (link, idx) => {
+          const linkPromise = new Promise(async (resolve, reject) => {
+            let response = null;
+            try {
+              response = await fetch(link);
+            } catch(err) {
+              console.log(err);
+            }
+            const blob = await response.blob();
+            let dataType = link.split(".")[3]
+            if(dataType.length > 4) {
+              dataType = dataType.split("?")[0]
+            }
+            resolve({data: blob, type: dataType });
+          });
+          promises.push(linkPromise);
 
-        return false;
+          if(idx === response.gallery.length - 1 ) {
+            props.setLoading("Saving files...")
+            Promise.all(promises)
+            .then(data => {
+              data.forEach((blob, idx) => zip.file(`${idx}.${blob.type}`, blob.data))
+            })
+            .then(data => {
+              props.setLoading(null);
+              zip.generateAsync({type:"blob"})
+                .then(content => {
+                  setQueryStr("");
+                  saveAs(content, `${response.name}-gallery.zip`)
+                })
+            })
+          }
+        });
+      }
       });
   };
 
-  return (
-    <div
-      className="input-group mb-3"
-      style={{ cursor: props.loading ? "progress" : "default" }}
-    >
-      <input
-        type="text"
-        className="form-control form-control-lg"
-        placeholder="Toyhouse Link"
-        value={queryStr}
-        onChange={(e) => setQueryStr(e.target.value)}
-      />
+  return(
+    <div className="input-group mb-3" style={{cursor: props.loading ? "progress" : "default"}}>
+      <input type="text" className="form-control form-control-lg" placeholder="Toyhouse Link"
+        value={queryStr} onChange={(e) => setQueryStr(e.target.value)} />
       <div className="input-group-append">
-        <button
-          className="btn btn-outline-primary btn-lg"
-          type="button"
-          disabled={props.loading || !props.isOnline}
-          onClick={handleSubmit}
-        >
-          Download
-        </button>
+        <button className="btn btn-outline-primary btn-lg" type="button" 
+          disabled={props.loading || !props.isOnline} onClick={handleSubmit}>Download</button>
       </div>
     </div>
-  );
+  )
 };
 
 export default AppForm;
