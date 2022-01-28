48b449702b63f6b375ce10a176a50bbc775c8b5e
Lily <lolpo12345@outlook.com>
2022-01-29 00:14:47 +0300

    Revert "Add spinner to loading text"
    This reverts commit b73368844a0fe37964dae7eafdf5e517b5067920.

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,20 +1,11 @@
-import { Spinner } from "react-bootstrap";
 
 const Loading = (props) => {
-  return (
-    <div
-      className="text-primary"
-      style={{ display: props.loading ? "block" : "none" }}
-    >
+
+  return(
+    <div className="text-primary" style={{display: props.loading ? "block" : "none"}}>
       {props.loading ? props.loading : null}
-      {props.loading && props.loading !== "Gallery downloaded!" ? (
-        <Spinner
-          animation="border"
-          style={{ width: 20, height: 20, fontSize: 10, marginLeft: 5 }}
-        />
-      ) : null}
     </div>
-  );
+  )
 };
 
 export default Loading;
