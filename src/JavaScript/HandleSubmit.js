1a67714bc1625a583f80eea1c5ea3f5182f67afb
Lily <lolpo12345@outlook.com>
2022-02-15 01:47:19 +0300

    Move JavaScript code to its own file

---
+++ src/JavaScript/HandleSubmit.js
@@ -0,0 +1,73 @@
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+
+const HandleSubmit = async (props, queryStr) => {
+  props.setHasError(false);
+
+  let id = null;
+  if (!queryStr.startsWith("https") && !queryStr.startsWith("toyhou.se")) {
+    props.setHasError("Please paste in a valid Toyhouse link!");
+    return;
+  } else if (queryStr.startsWith("toyhou.se")) {
+    id = queryStr.split("/")[1];
+  } else if (queryStr.startsWith("https://toyhou.se")) {
+    id = queryStr.split("/")[3];
+  } else {
+    props.setHasError("Please paste in a valid Toyhouse link!");
+    return;
+  }
+
+  props.setLoading("Downloading images...");
+  fetch(
+    `https://toyhouse-rails-api.herokuapp.com/character/?id=${id}&gallery_only=true`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      if (response.status === 422 || response.status === 404) {
+        props.setLoading(null);
+        props.setHasError(response.msg);
+      } else {
+        props.setLoading("Handling the gallery...");
+        let zip = new JSZip();
+        const promises = [];
+
+        response.gallery.forEach(async (link, idx) => {
+          const linkPromise = new Promise(async (resolve, reject) => {
+            let response = null;
+            try {
+              response = await fetch(link);
+            } catch (err) {
+              console.log(err);
+            }
+            const blob = await response.blob();
+            let dataType = link.split(".")[3];
+            if (dataType.length > 4) {
+              dataType = dataType.split("?")[0];
+            }
+            resolve({ data: blob, type: dataType });
+          });
+          promises.push(linkPromise);
+
+          if (idx === response.gallery.length - 1) {
+            props.setLoading("Saving files...");
+            Promise.all(promises)
+              .then((data) => {
+                data.forEach((blob, idx) =>
+                  zip.file(`${idx}.${blob.type}`, blob.data)
+                );
+              })
+              .then((data) => {
+                props.setLoading(null);
+                zip.generateAsync({ type: "blob" }).then((content) => {
+                  saveAs(content, `${response.name}-gallery.zip`);
+                });
+              });
+          }
+        });
+      }
+    });
+};
+
+export default HandleSubmit;
