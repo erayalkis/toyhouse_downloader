eaec4ba42f3eb3299ea8268aeae707629e40683b
Lily <lolpo12345@outlook.com>
2022-02-15 01:35:19 +0300

    Update Limitations.jsx

--- src/Components/Limitations.jsx
+++ src/Components/Limitations.jsx
@@ -1,19 +1,18 @@
-
 const Limitations = () => {
-
-  return(
+  return (
     <div className="mt-5">
       <h1 className="display-4 text-center">Limitations for this app</h1>
-      <div className="mx-auto mt-4" style={{width:500}}>
+      <div className="mx-auto mt-4" style={{ width: 500 }}>
         <ul>
-          <li>This app can't fetch galleries for locked (private) profiles.</li>
+          <li>
+            This app will not fetch galleries for locked (private) profiles.
+          </li>
           <li>This app can't fetch images that have NSFW tags on them.</li>
-          <li>This app can't fetch galleries that have custom designs.</li>
-        </ul>   
+          <li>This app might not fetch galleries that have custom HTML.</li>
+        </ul>
       </div>
-      
     </div>
-  )
+  );
 };
 
 export default Limitations;
