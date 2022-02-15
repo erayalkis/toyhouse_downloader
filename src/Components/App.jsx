d9ae3b0ee6c47feb00e0d5968abc6f6b9b568b4a
Lily <lolpo12345@outlook.com>
2022-02-15 20:34:51 +0300

    Create queueWindow

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -16,48 +16,50 @@ function App() {
 
   return (
     <div>
-      <AppStatus
-        isOnline={isOnline}
-        setIsOnline={setIsOnline}
-        loading={loading}
-        setLoading={setLoading}
-        fetching={fetching}
-        setFetching={setFetching}
-        setError={setError}
-      />
       <QueueWindow />
-      <div className="container text-center mt-4 w-75 body-bg-main mt-4">
-        <div className="card p-5">
-          <p className="display-3">Toyhouse Downloader</p>
-          <p>Mass download galleries directly from Toyhouse</p>
-          <AppForm
-            setHasError={setError}
-            setLoading={setLoading}
-            loading={loading}
-            isOnline={isOnline}
-          />
-          <Error error={error} />
-          <Loading loading={loading} />
-          <p
-            className="text-primary"
-            style={{ display: !error && !loading ? "block" : "none" }}
-          >
-            Welcome! Paste a Toyhouse link in the input field to get started!
-          </p>
+      <div>
+        <AppStatus
+          isOnline={isOnline}
+          setIsOnline={setIsOnline}
+          loading={loading}
+          setLoading={setLoading}
+          fetching={fetching}
+          setFetching={setFetching}
+          setError={setError}
+        />
+        <div className="container text-center mt-4 w-75 body-bg-main mt-4">
+          <div className="card p-5">
+            <p className="display-3">Toyhouse Downloader</p>
+            <p>Mass download galleries directly from Toyhouse</p>
+            <AppForm
+              setHasError={setError}
+              setLoading={setLoading}
+              loading={loading}
+              isOnline={isOnline}
+            />
+            <Error error={error} />
+            <Loading loading={loading} />
+            <p
+              className="text-primary"
+              style={{ display: !error && !loading ? "block" : "none" }}
+            >
+              Welcome! Paste a Toyhouse link in the input field to get started!
+            </p>
+          </div>
+
+          <small className="opacity-50">
+            <em>
+              All content belongs to Cyan Crows and the respective artists. I
+              claim no ownership over the downloaded content.
+            </em>
+          </small>
         </div>
 
-        <small className="opacity-50">
-          <em>
-            All content belongs to Cyan Crows and the respective artists. I
-            claim no ownership over the downloaded content.
-          </em>
-        </small>
-      </div>
-
-      <div>
-        <hr className="w-75 mx-auto" />
-        <Limitations />
-        <Footer />
+        <div>
+          <hr className="w-75 mx-auto" />
+          <Limitations />
+          <Footer />
+        </div>
       </div>
     </div>
   );
--- src/Components/QueueWindows.jsx
+++ src/Components/QueueWindows.jsx
@@ -1,5 +1,19 @@
+import "../Stylesheets/QueueWindow.css";
+
 const QueueWindow = () => {
-  return <div>q</div>;
+  return (
+    <div>
+      <div className="queue-bg"></div>
+      <div className="queue-window">
+        <p className="display-6 queue-header">Character Queue</p>
+
+        <div className="queue-characters">
+          <div className="character">Poopie</div>
+          <div className="character">Testie</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QueueWindow;
