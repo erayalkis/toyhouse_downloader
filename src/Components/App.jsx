146a3589a1aea0dd708ca5d446e33928c7c38b27
Lily <lolpo12345@outlook.com>
2022-02-15 14:31:06 +0300

    Create QueueWindow

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import AppStatus from './AppStatus';
-import AppForm from './AppForm';
-import Error from './Error';
-import Loading from './Loading';
-import Footer from './Footer';
-import Limitations from './Limitations';
+import { useState } from "react";
+import AppStatus from "./AppStatus";
+import AppForm from "./AppForm";
+import Error from "./Error";
+import Loading from "./Loading";
+import Footer from "./Footer";
+import Limitations from "./Limitations";
 
 function App() {
   const [error, setError] = useState(null);
@@ -14,26 +14,43 @@ function App() {
 
   return (
     <div>
-    <AppStatus isOnline={isOnline} setIsOnline={setIsOnline} loading={loading} setLoading={setLoading} 
-      fetching={fetching} setFetching={setFetching} setError={setError} />
-    <div className="container text-center mt-4 w-75 body-bg-main mt-4">
-      <div className="card p-5">
-        <p className="display-3">Toyhouse Downloader</p>
-        <p>Mass download galleries directly from Toyhouse</p>
-        <AppForm setHasError={setError} setLoading={setLoading} loading={loading} isOnline={isOnline} />
-        <Error error={error} />
-        <Loading loading={loading} />
-        <p className="text-primary" style={{display: !error && !loading ? "block" : "none"}}>
-            Welcome! Paste a Toyhouse link in the input field to get started!</p>
+      <AppStatus
+        isOnline={isOnline}
+        setIsOnline={setIsOnline}
+        loading={loading}
+        setLoading={setLoading}
+        fetching={fetching}
+        setFetching={setFetching}
+        setError={setError}
+      />
+      <div className="container text-center mt-4 w-75 body-bg-main mt-4">
+        <div className="card p-5">
+          <p className="display-3">Toyhouse Downloader</p>
+          <p>Mass download galleries directly from Toyhouse</p>
+          <AppForm
+            setHasError={setError}
+            setLoading={setLoading}
+            loading={loading}
+            isOnline={isOnline}
+          />
+          <Error error={error} />
+          <Loading loading={loading} />
+          <p
+            className="text-primary"
+            style={{ display: !error && !loading ? "block" : "none" }}
+          >
+            Welcome! Paste a Toyhouse link in the input field to get started!
+          </p>
+        </div>
+
+        <small className="opacity-50">
+          <em>
+            All content belongs to Cyan Crows and the respective artists. I
+            claim no ownership over the downloaded content.
+          </em>
+        </small>
       </div>
 
-      <small className="opacity-50">
-        <em>
-          All content belongs to Cyan Crows and the respective artists. We claim no ownership over the downloaded content.
-        </em>
-      </small>
-    </div>
-    
       <div>
         <hr className="w-75 mx-auto" />
         <Limitations />
---
+++ src/Components/QueueWindows.jsx
@@ -0,0 +1,3 @@
+const QueueWindow = () => {};
+
+export default QueueWindow;
