3e6bd7fc71909ee03ed12af30a60ea28507ccce5
Lily <lolpo12345@outlook.com>
2022-02-18 01:29:54 +0300

    Add colour to bg for characters
    Make visibility better

--- src/Components/Characters.jsx
+++ src/Components/Characters.jsx
@@ -5,7 +5,7 @@ const Characters = (props) => {
   const { queue } = props;
 
   return (
-    <div className="w-75 h-100 overflow-scroll">
+    <div className="w-75 h-100 overflow-scroll characters">
       {queue.map((character) => (
         <Character
           key={character.id}
