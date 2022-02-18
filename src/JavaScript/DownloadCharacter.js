a9510ae7cf6a78248a382dbb378bc790d8cd37d8
Lily <lolpo12345@outlook.com>
2022-02-18 03:21:53 +0300

    Remove unnecessary console.logs

--- src/JavaScript/DownloadCharacter.js
+++ src/JavaScript/DownloadCharacter.js
@@ -3,24 +3,19 @@ import JSZip from "jszip";
 import { saveAs } from "file-saver";
 
 const DownloadCharacter = (id, setQueue) => {
-  console.log(`Fetching ${id}`);
   fetch(
     `https://toyhouse-rails-api.herokuapp.com/character/?id=${id}&gallery_only=true`
   )
     .then((response) => {
-      console.log(`Response received for ${id}`);
       return response.json();
     })
     .then((response) => {
       if (response.status === 422 || response.status === 404) {
-        console.log("Something went wrong :(");
       } else {
-        console.log(`Zipping ${id}`);
         let zip = new JSZip();
         const promises = [];
 
         response.gallery.forEach((link, idx) => {
-          console.log(`Creating promises for ${id}`);
           const linkPromise = CreatePromise(link);
           promises.push(linkPromise);
 
@@ -32,7 +27,6 @@ const DownloadCharacter = (id, setQueue) => {
                 );
               })
               .then((data) => {
-                console.log(`Saving ${id}`);
                 zip.generateAsync({ type: "blob" }).then((content) => {
                   saveAs(content, `${response.name}-gallery.zip`);
                   setQueue((old) =>
