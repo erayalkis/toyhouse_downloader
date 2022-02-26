81c58664d9df198eefc7cdd02fade35eebc26cf8
Lily <lolpo12345@outlook.com>
2022-02-27 02:34:08 +0300

    Start switching to contexts

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,14 @@ import App from "./Components/App";
 import QueueContext from "./Contexts/QueueContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
+import { useContext } from "react";
+import LoadingContext from "./Contexts/LoadingContext";
+import ErrorContext from "./Contexts/ErrorContext";
 
 const Index = () => {
   const [queue, setQueue] = useState([]);
+  const [loading, setLoading] = useContext(LoadingContext);
+  const [error, setError] = useContext(ErrorContext);
 
   return (
     <QueueContext.Provider value={{ queue, setQueue }}>
