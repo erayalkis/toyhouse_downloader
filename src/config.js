50eda07ba3821b3922563d3522ca0882d83331d8
Lily <lolpo12345@outlook.com>
2022-05-05 16:12:15 +0300

    Update config.backend_url's port

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = global.config = {
   backend_url:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
+      ? "http://localhost:3001"
       : "https://toyhouse-rails-api.herokuapp.com",
 };
