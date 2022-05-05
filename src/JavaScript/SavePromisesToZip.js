32171f639a939a9e212360a6a21b89b3895a4a38
Lily <lolpo12345@outlook.com>
2022-05-05 16:13:10 +0300

    Use reducer intstead of forEach to save artist creds

--- src/JavaScript/SavePromisesToZip.js
+++ src/JavaScript/SavePromisesToZip.js
@@ -2,13 +2,20 @@ import JSZip from "jszip";
 
 const SavePromisesToZip = async (promises) => {
   let promise = new Promise(async (resolve) => {
-    let zip = new JSZip();
     const data = await Promise.all(promises);
+    let zip = new JSZip();
 
-    data.forEach((blob, idx) => {
-      if (blob) zip.file(`${idx}.${blob.type}`, blob.data);
-    });
+    let str = data.reduce((str, blob, idx) => {
+      if (blob) {
+        zip.file(`${idx}.${blob.type}`, blob.data);
+        console.log(blob);
+        str += `${idx}. ${blob.artist.name} [ ${blob.artist.profile} ]\n`;
+      }
 
+      return str;
+    }, "");
+
+    zip.file("credits.txt", str);
     resolve(zip);
   });
 
