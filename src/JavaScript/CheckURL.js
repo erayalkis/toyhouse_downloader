fcb360abef73a882fd554df9bba7f92459c56ed8
Lily <lolpo12345@outlook.com>
2022-05-08 01:17:09 +0300

    Add /gallery check in url to prevent duplicated routes

--- src/JavaScript/CheckURL.js
+++ src/JavaScript/CheckURL.js
@@ -7,10 +7,17 @@ const useURLCheck = async (queryStr) => {
   const split = queryStr.split("/");
 
   if (queryStr.startsWith("toyhou.se")) {
-    id = split[1] + (split[2] != null ? `/${split[2]}` : ""); // If tab ID exists, append it to the current id
+    console.log(split[2]);
+    id =
+      split[1] +
+      (split[2] != null && split[2] !== "gallery" ? `/${split[2]}` : ""); // If tab ID exists, append it to the current id
     return id;
   } else if (queryStr.startsWith("https://toyhou.se")) {
-    id = split[3] + (split[4] != null ? `/${split[4]}` : ""); // Same as above
+    console.log(split[4]);
+
+    id =
+      split[3] +
+      (split[4] != null && split[4] !== "gallery" ? `/${split[4]}` : ""); // Same as above
     return id;
   } else {
     return null;
