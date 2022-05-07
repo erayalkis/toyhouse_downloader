9d6f792b8bc13cf2ee0f01a4a77e48f82a5d4326
Lily <lolpo12345@outlook.com>
2022-05-08 01:09:12 +0300

    Add support for character tabs

--- src/JavaScript/CheckURL.js
+++ src/JavaScript/CheckURL.js
@@ -2,11 +2,15 @@ const useURLCheck = async (queryStr) => {
   let id = null;
   if (!queryStr.startsWith("https") && !queryStr.startsWith("toyhou.se")) {
     return null;
-  } else if (queryStr.startsWith("toyhou.se")) {
-    id = queryStr.split("/")[1];
+  }
+
+  const split = queryStr.split("/");
+
+  if (queryStr.startsWith("toyhou.se")) {
+    id = split[1] + (split[2] != null ? `/${split[2]}` : ""); // If tab ID exists, append it to the current id
     return id;
   } else if (queryStr.startsWith("https://toyhou.se")) {
-    id = queryStr.split("/")[3];
+    id = split[3] + (split[4] != null ? `/${split[4]}` : ""); // Same as above
     return id;
   } else {
     return null;
