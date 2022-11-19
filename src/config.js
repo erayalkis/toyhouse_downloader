fe9653985172a32883d30052d8a1be0416cc6a7b
Eray Alkış <lolpo12345@outlook.com>
2022-11-20 00:16:24 +0300

    Update live server URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = global.config = {
   backend_url:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8080"
-      : "https://toyhouse-rails-api.herokuapp.com",
+      : "https://toyhouse-api.onrender.com",
 };
