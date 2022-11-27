8dbc48ccfaaa3cd43129a86ef6caedfe63ab1e76
Eray Alkış <lolpo12345@outlook.com>
2022-11-28 02:00:29 +0300

    Disable debug mode on mixpanel

--- src/JavaScript/Mixpanel.js
+++ src/JavaScript/Mixpanel.js
@@ -1,6 +1,6 @@
 import mixpanel from "mixpanel-browser";
 
-mixpanel.init('c87ddd59f6686afdf4945c6da11103a0', { debug: true })
+mixpanel.init('c87ddd59f6686afdf4945c6da11103a0')
 
 let actions = {
   track: (name, props) => {
