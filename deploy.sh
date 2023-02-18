48400fed6127c5b152c785a2a5614bd24a83a418
Eray Alkış <lolpo12345@outlook.com>
2023-02-18 23:47:55 +0300

    Add log messages for build phase

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,17 @@ RED='\033[0;31m'
 L_RED='\033[1;31m'
 NC='\033[0m'
 
-echo "debug test"
-echo "[ ${BLUE}INFO${NC} ] INFO"
-echo "[ ${GREEN}OK${NC} ] OK"
-echo "[ ${RED}ERR${NC} ] ERROR"
+# echo "debug test"
+# echo "[ ${BLUE}INFO${NC} ] INFO"
+# echo "[ ${GREEN}OK${NC} ] OK"
+# echo "[ ${RED}ERR${NC} ] ERROR"
 # set -e
 
-echo "[ ${BLUE}INFO${NC} ] STARTING DEPLOY"
+printf "\n[ ${BLUE}INFO${NC} ] BUILDING APP\n"
 
-# npm run build
+npm run build && printf "\n[ ${GREEN}OK${NC} ] BUILD COMPLETE\n"
+
+printf "\n[ ${BLUE}INFO${NC} ] PUSHING GIT SUBTREE\n"
 
 # git add dist
 # git commit -m 'Add dist subtree'
