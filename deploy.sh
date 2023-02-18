4c1cb6d16104185a6036bf7efa7cb35b7c749586
Eray Alkış <80722863+erayalkis@users.noreply.github.com>
2023-02-19 01:59:43 +0300

    V2 Fixes (#3)
    * Remove v1 files
    
    * Create base for v2
    
    * Delete generated files (missing router)
    
    * Generate new scaffold for v2
    
    * Create base
    
    * Create base structure
    
    * Install tailwind
    
    * Create base
    
    * Implement base design for main card
    
    * Create base for notice box
    
    * Automate notices
    
    * Finish notices
    
    * Set up nav and footer
    
    * Add slots to notices
    
    * Create base logic
    
    * Create base for status display
    
    * Display errors and messages
    
    * Finish app status check
    
    * Display app status
    
    * Add basic queue div
    
    * Finish basic blobification
    
    * Create basic download
    
    * Add check for ?'s in url
    
    * Create basic queue
    
    * Wrap up queue for today
    
    * Start testing new layout
    
    * Finish new layout
    
    * Set up sizing for queues
    
    * Style queue download button
    
    * Finish queue downloads
    
    * Add additional checks
    
    * Update notices
    
    * Update promise to include artist data
    
    * Add space
    
    * Reduce padding on queue
    
    * Add credits file
    
    * Add metadata
    
    * Prettify credits file
    
    * Add URL check
    
    * Add support for multiple character ID's
    
    * Make text small on phones
    
    * Add functionality for removing character from queue
    
    * Remove console.logs
    
    * Run linter
    
    * Fix final errors
    
    * Create base deploy script
    
    * Add log messages for build phase
    
    * Update favicon
    
    * Finalize base for deploy script
    
    * Add -f to push in deploy.sh
    
    * Add dist subtree
    
    * Fix deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+BLUE='\033[0;34m'
+L_BLUE='\033[1;34m'
+GREEN='\033[0;32m'
+L_GREEN='\033[1;32m'
+RED='\033[0;31m'
+L_RED='\033[1;31m'
+NC='\033[0m'
+
+# echo "debug test"
+# echo "[ ${BLUE}INFO${NC} ] INFO"
+# echo "[ ${GREEN}OK${NC} ] OK"
+# echo "[ ${RED}ERR${NC} ] ERROR"
+# set -e
+
+printf "\n[ ${BLUE}INFO${NC} ] STARTING BUILDING PROCESS\n"
+
+npm run build && printf "\n[ ${GREEN}OK${NC} ] BUILD COMPLETE\n"
+
+printf "\n[ ${BLUE}INFO${NC} ] PUSHING GIT SUBTREE\n"
+
+cd dist
+git init
+git add -A
+git commit -m 'Deploy commit'
+git push -f git@github.com:erayalkis/toyhouse_downloader.git master:gh-pages && "\n[ ${GREEN}OK${NC} ] PUSH SUCCESSFUL\n"
+cd -
+
+printf "\n[ ${BLUE}INFO${NC} ] VISIT https://erayalkis.github.io/toyhouse_downloader TO VIEW THE CHANGES\n"
+printf "[ ${BLUE}INFO${NC} ] (MAY TAKE A COUPLE OF MINUTES TO UPDATE)\n"
